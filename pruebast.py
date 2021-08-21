095490f8bdc99761d7010a88dfb2b98c70ca42cf
astomelio <68124228+astomelio@users.noreply.github.com>
2021-08-21 08:25:08 -0500

    nuevo analisis

--- pruebast.py
+++ pruebast.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from statistics import mean
 from statistics import median
+import plotly.express as px
 
 info = pd.read_json('ws.json')
 devs = []
@@ -16,17 +17,34 @@ info["cambio"] = camb
 camb_total = []
 camb_final = []
 cambio_real = []
+derivadas1 = []
+derivadas2 = []
+zerodev1 = []
+zerodev2 = []
 for x in range(0, len(info.prices)):
+    derivada1 = [((n - info.prices[x][0]) / (n + 1)) * 100 for n in info.prices[x]]
+    derivada2 = [((int(derivada1[n]) - int(derivada1[0])) / (n + 1)) * 100 for n in range(0, len(derivada1))]
     camb_tot = [((n - info.prices[x][0]) / (info.prices[x][0])) * 100 for n in info.prices[x]]
     camb_real = [((info.prices[x][n] - info.prices[x][n - 15]) / (info.prices[x][n - 15])) * 100 for n in
                  range(15, len(info.prices[x]) - 1)]
+    derivada1 = np.array(derivada1)
+    derivada2 = np.array(derivada2)
+    zerosdev1 = np.where(derivada1 == 0)
+    zerosdev2 = np.where(derivada2 == 0)
     camb_total.append(camb_tot)
     camb_final.append(camb_tot[-1])
     cambio_real.append(camb_real)
+    derivadas1.append(derivada1)
+    derivadas2.append(derivada2)
+    zerodev1.append(zerosdev1)
+    zerodev2.append(zerosdev2)
 info['resta'] = camb_total
 info['camb_final'] = camb_final
 info['cambio_real'] = cambio_real
-
+info['derivada1'] = derivadas1
+info['derivada2'] = derivadas2
+info['zerosdev1'] = zerodev1
+info['zerosdev2'] = zerodev2
 df = pd.read_csv('result.csv')
 df['time_entry'] = pd.to_datetime(df['time_entry'], unit='ms').round('min')
 info['time'] = pd.to_datetime(info['time']).round('min')
@@ -79,6 +97,7 @@ df_cero['cambiot_real'] = cambiost_real
 lismax = []
 proms = []
 medianas = []
+# df cero solo donde hay cambio de signo
 for i in df_cero.cambiot_real:
     maximo = max(i)
     prom = mean(i)
@@ -96,25 +115,30 @@ precios = []
 transacciones = []
 lados = []
 pnls = []
-camb2 = []
-camb4 = []
+precios_final = []
+precios_30 = []
+deriva = []
 n = 0
 for pos in range(0, len(df_cero.cambiot_real)):
     for i in range(0, len(df_cero.cambiot_real[pos])):
 
-        if (df_cero.cambiot_real[pos][i] > 35):
+        if (df_cero.cambiot_real[pos][i] > 30):
             posicion = df_cero.zero_real[pos][i + 1]
             pre = importante.resta[pos][posicion + 15]
             segs = df_cero.cambiot_real[pos][i]
-            cambi = df_cero.derivada_real[pos][i]
+            cambi = df_cero.derivada_real[pos][i + 1]
             try:
-                cambi2 = df_cero.derivada_real[pos][i + 5]
+                cambio2 = importante['cambio_real'][pos][posicion + 15]
             except:
-                cambi2 = 0
+                cambio2 = 0
             try:
-                cambi4 = df_cero.derivada_real[pos][i + 10]
+                precio_final = importante.resta[pos][posicion + segs + 14]
             except:
-                cambi4 = 0
+                precio_final = 0
+            try:
+                precio_30 = importante.resta[pos][posicion + 30 + 15]
+            except:
+                precio_30 = 0
 
             algo = df_cero.zero_real[pos][i + 1] + 15
             lado = importante.side2[pos]
@@ -127,8 +151,9 @@ for pos in range(0, len(df_cero.cambiot_real)):
             transacciones.append(transacion)
             lados.append(lado)
             pnls.append(pnl)
-            camb2.append(cambi2)
-            camb4.append(cambi4)
+            precios_final.append(precio_final)
+            precios_30.append(precio_30)
+            deriva.append(cambio2)
 
     n += 1
 
@@ -140,5 +165,12 @@ df_transacciones['seg_real'] = algos
 df_transacciones['dev'] = precios
 df_transacciones['side'] = lados
 df_transacciones['Pnl'] = pnls
-df_transacciones['camb2'] = camb2
-df_transacciones['camb4'] = camb4
+df_transacciones['precio_final'] = precios_final
+df_transacciones['precio_30'] = precios_30
+df_transacciones['derivadas'] = deriva
+
+pd_graph = pd.DataFrame()
+pd_graph['precios'] = importante.prices[0]
+pd_graph['index1'] = pd_graph.index
+fig = px.line(pd_graph, x="index1", y="precios", title='precio vs tiempo')
+fig.show()
